1947249b1a5f4289c07b427f2360ae7b61392086
Oyebanji Olayemi Afolarin <olayemioyebanji911@gmail.com>
2020-01-12 13:15:44 +0100

    update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 if('serviceWorker' in navigator) {
   navigator.serviceWorker
-           .register('/sw.js')
+           .register('/addtoscreen/sw.js')
            .then(function() { console.log('Service Worker Registered'); });
 }
 
