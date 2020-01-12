17d9e0f7d58c6bb4f07c8b5bae6d74678c464ba5
Oyebanji Olayemi Afolarin <olayemioyebanji911@gmail.com>
2020-01-12 13:09:24 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('video-store').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/index.js',
+       '/style.css',
+       '/dalepersonalcard.jpg',
+       '/images',
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
